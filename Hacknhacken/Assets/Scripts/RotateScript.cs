e4c06a43015154c33ec44dc3f0ee3e0f360843c7
dhohmann <dtwin95@gmx.de>
2018-03-10 14:17:17 +0100

    Base support for startup rotation
    - Updated rotation script
    - Added tag Enemy

--- Hacknhacken/Assets/Scripts/RotateScript.cs
+++ Hacknhacken/Assets/Scripts/RotateScript.cs
@@ -8,8 +8,20 @@ public class RotateScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Vector3 rotation = new Vector3(0, 0, rotationAngle);
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach(GameObject obj in enemies)
+        {
+            obj.transform.eulerAngles = rotation;
+        }
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.eulerAngles = rotation;
+
+        Camera camera = Camera.main;
+        camera.transform.eulerAngles = rotation;
+    }
 	
 	// Update is called once per frame
 	void Update () {
